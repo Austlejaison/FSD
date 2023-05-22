9bcae85db1c4474911800c5b62e1bb72f028dd4a
Austlejaison <109846481+Austlejaison@users.noreply.github.com>
2023-05-22 15:13:04 +0530

    Add files via upload

---
+++ VS/aj.js
@@ -0,0 +1,42 @@
+let classess = [{
+    name:"Raj",
+    age:45,
+    place:"Kannur"
+},
+{
+    name:"krishna",
+    age:42,
+    place:"Kochi" 
+}];
+console.log(classess);
+console.log(classess[1].name);
+function multi(a,b)
+{
+    console.log(a*b);
+};
+multi(5,10);
+const add =(x,y)=>
+{
+    console.log(x+y);
+}
+add(3,5);
+let c=20;
+let d=10;
+if(c>d)
+{
+    console.log(true);  
+}
+else{
+    console.log(false); 
+}
+
+for(i=1;i<=5;i++)
+{
+    console.log(i); 
+}
+let j=1;
+do{
+    console.log(j);
+    j++;
+}
+while(j<=5)
---
+++ VS/script.js
@@ -0,0 +1,26 @@
+console.log("Hai Hello");
+let a=500;
+let b=500;
+console.log(a-b);
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
+
+let person={
+    name:"Lisa",
+    age:18,
+    place:"Kollam",
+}
+console.log(person.name,person.age,person.place);
+console.log(`${(person.name)} lives in ${(person.place)}`);
+let numbers=[1,2,3,4,5];
+numbers.push("6");
+console.log(numbers);
+numbers.shift();
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
